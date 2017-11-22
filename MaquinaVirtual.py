e23809e5d640f9344179cbc72bef549211ac035c
Daniel Rodriguez <danyrod94@gmail.com>
2017-11-22 01:08:06 -0600

    nuevo meme o algo

--- MaquinaVirtual.py
+++ MaquinaVirtual.py
@@ -465,6 +465,7 @@ def f_gosub():
 
 	print("Saltamos al cuadruplo " + str(counter))
 
+
 def f_return():
 	global counterGo
 	global counter
@@ -472,15 +473,20 @@ def f_return():
 	keyParamPosition = len(keyParams) - 1
 	PositionMemories = len(memories) - 1
 
-	if(isTemporal(quadruples[counter][1])):
-		firstKey ="temporals"
-	else:
-		firstKey ="variables"
-		if(not isLocal(quadruples[counter][1])):
-			PositionMemories = 0
+	
+	#Revisar si el return tiene un none o una expression
+	if(quadruples[counter][1] != None):
+		
+		if(isTemporal(quadruples[counter][1])):
+			firstKey ="temporals"
+		else:
+			firstKey ="variables"
+			if(not isLocal(quadruples[counter][1])):
+				PositionMemories = 0
 
-	#almacenar expresion resultante en la variable global de la funcion
-	memories[0]['variables'][keyParams[keyParamPosition]].value = memories[PositionMemories][firstKey][quadruples[counter][1]].value
+
+		#almacenar expresion resultante en la variable global de la funcion
+		memories[0]['variables'][keyParams[keyParamPosition]].value = memories[PositionMemories][firstKey][quadruples[counter][1]].value
 
 	#remover la ultima llave de block y contador de parametro
 	counterParams.pop()
